8bb4a0bdc8f2085adb6fbb98e176ca723dc76b66
hostoria <ekellempoy@gmail.com>
2025-05-06 14:07:56 +0800

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,24 @@
+window.userData = {
+  adhost: {
+    nama: "Adhost",
+    status: "Developers",
+    nomor: "6283133675770",
+    ram: "1GB",
+    disk: "2GB",
+    cpu: "100%",
+    tanggal: "2025-05-06",
+    waktuPakai: 1,
+    server: "RunBot"
+  },
+  admud: {
+    nama: "Admud",
+    status: "Developers",
+    nomor: "6283871005242",
+    ram: "2GB",
+    disk: "2GB",
+    cpu: "200%",
+    tanggal: "2025-05-06",
+    waktuPakai: 2,
+    server: "Bedrock"
+  }
+};
